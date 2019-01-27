06e5e8a55e10366d90f535687e259d6c1dc0bf5b
zhxiaom5 <zhxiaom5@gmail.com>
2019-01-27 14:25:36 +0800

    change test js and lua

--- filebeat/fb.js
+++ filebeat/fb.js
@@ -1,13 +1,9 @@
 		console.log("Javascript engine loaded "); 
-		process = function(fields){
-			fields.message = fields.syslog_message
+		process = function (fields){
+			addfields={}
+			addfields.javascript="zhxiaom5"
 			console.log(JSON.stringify(fields))
-fields.attributes={}
-fields.attributes.text = "just some Javascript code"
-fields.metrics={}
-fields.metrics."outside.temperature"=34.5
-			fields.javascript="running in GO!"
 //			var d = new Date(fields.timestamp)
 //			console.log(" date " + d)
-			return fields
+			return addfields
 		}
---
+++ filebeat/fb.lua
@@ -0,0 +1,6 @@
+print("lua file loaded")
+function process(fields)
+    local addfields={}
+    addfields.luascript="zhxiaom"
+	return addfields
+end
